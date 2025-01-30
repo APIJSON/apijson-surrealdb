7bb0e6d40b0b732ee92ec3f649ad5d279c388899
TommyLemon <tommylemon@qq.com>
2025-01-30 11:18:48 +0800

    初步完成查询

---
+++ README.md
@@ -0,0 +1,127 @@
+# apijson-surrealdb  [![](https://jitpack.io/v/APIJSON/apijson-surrealdb.svg)](https://jitpack.io/#APIJSON/apijson-surrealdb)
+腾讯 [APIJSON](https://github.com/Tencent/APIJSON) 6.1.0+ 的 SurrealDB 数据库插件，可通过 Maven, Gradle 等远程依赖。<br />
+An SurrealDB plugin for Tencent [APIJSON](https://github.com/Tencent/APIJSON) 6.1.0+
+
+![image](https://github.com/APIJSON/apijson-surrealdb/assets/5738175/243d7a46-e035-4fe6-be63-51cb54d4a69d)
+![image](https://github.com/APIJSON/apijson-surrealdb/assets/5738175/3c2919b6-f90a-4a9e-9fb7-592eb4c1a6bb)
+
+
+## 添加依赖
+## Add Dependency
+
+### Maven
+#### 1. 在 pom.xml 中添加 JitPack 仓库
+#### 1. Add the JitPack repository to pom.xml
+```xml
+	<repositories>
+		<repository>
+		    <id>jitpack.io</id>
+		    <url>https://jitpack.io</url>
+		</repository>
+	</repositories>
+```
+
+![image](https://user-images.githubusercontent.com/5738175/167261814-d75d8fff-0e64-4534-a840-60ef628a8873.png)
+
+<br />
+
+#### 2. 在 pom.xml 中添加 apijson-surrealdb 依赖
+#### 2. Add the apijson-surrealdb dependency to pom.xml
+```xml
+	<dependency>
+	    <groupId>com.github.APIJSON</groupId>
+	    <artifactId>apijson-surrealdb</artifactId>
+	    <version>LATEST</version>
+	</dependency>
+```
+
+<br />
+
+https://github.com/APIJSON/APIJSON-Demo/blob/master/APIJSON-Java-Server/APIJSONBoot-MultiDataSource/pom.xml
+
+<br />
+<br />
+
+### Gradle
+#### 1. 在项目根目录 build.gradle 中最后添加 JitPack 仓库
+#### 1. Add the JitPack repository in your root build.gradle at the end of repositories
+```gradle
+	allprojects {
+		repositories {
+			maven { url 'https://jitpack.io' }
+		}
+	}
+```
+<br />
+
+#### 2. 在项目某个 module 目录(例如 `app`) build.gradle 中添加 apijson-surrealdb 依赖
+#### 2. Add the apijson-surrealdb dependency in one of your modules(such as `app`)
+```gradle
+	dependencies {
+	        implementation 'com.github.APIJSON:apijson-surrealdb:latest'
+	}
+```
+
+<br />
+<br />
+<br />
+
+## 使用
+## Usage
+
+在你项目继承 AbstractSQLExecutor 的子类重写方法 execute <br/>
+Override execute in your SQLExecutor extends AbstractSQLExecutor
+
+```java
+        @Override
+        public JSONObject execute(@NotNull SQLConfig<Long> config, boolean unknownType) throws Exception {
+            if (config.isSurrealDB()) {
+                return SurrealDBUtil.execute(config, null, unknownType);
+            }
+   
+            return super.execute(config, unknownType);
+        }
+```
+
+<br/>
+在你项目继承 AbstractSQLConfig 的子类重写方法 execute <br/>
+Override execute in your SQLConfig extends AbstractSQLConfig
+
+```java
+	@Override
+    public String getNamespace() {
+        return SurrealDBUtil.getNamespace(super.getNamespace(), DEFAULT_NAMESPACE, isSurrealDB());
+    }
+
+    @Override
+    public String getSQLNamespace() {
+        return SurrealDBUtil.getSQLNamespace(super.getSQLNamespace(), isSurrealDB());
+    }
+        
+	@Override
+	public String getSchema() {
+		return SurrealDBUtil.getSchema(super.getSchema(), DEFAULT_SCHEMA, isSurrealDB());
+	}
+
+	@Override
+	public String getSQLSchema() {
+		return SurrealDBUtil.getSQLSchema(super.getSQLSchema(), isSurrealDB());
+	}
+```
+
+#### 见 [SurrealDBUtil](/src/main/java/apijson/surrealdb/SurrealDBUtil.java) 的注释及 [APIJSONBoot-MultiDataSource](https://github.com/APIJSON/APIJSON-Demo/blob/master/APIJSON-Java-Server/APIJSONBoot-MultiDataSource) 的 [DemoSQLExecutor](https://github.com/APIJSON/APIJSON-Demo/blob/master/APIJSON-Java-Server/APIJSONBoot-MultiDataSource/src/main/java/apijson/demo/DemoSQLExecutor.java) <br />
+
+#### See document in [SurrealDBUtil](/src/main/java/apijson/surrealdb/SurrealDBUtil.java) and [DemoSQLExecutor](https://github.com/APIJSON/APIJSON-Demo/blob/master/APIJSON-Java-Server/APIJSONBoot-MultiDataSource/src/main/java/apijson/demo/DemoSQLExecutor.java) in [APIJSONBoot-MultiDataSource](https://github.com/APIJSON/APIJSON-Demo/blob/master/APIJSON-Java-Server/APIJSONBoot-MultiDataSource)
+
+<br />
+<br />
+<br />
+
+有问题可以去 Tencent/APIJSON 提 issue <br />
+https://github.com/Tencent/APIJSON/issues/36
+
+<br /><br />
+
+#### 点右上角 ⭐Star 支持一下，谢谢 ^_^
+#### Please ⭐Star this project ^_^
+https://github.com/APIJSON/apijson-surrealdb
---
+++ src/main/java/apijson/surrealdb/SurrealDBUtil.java
@@ -0,0 +1,275 @@
+/*Copyright ©2024 APIJSON(https://github.com/APIJSON)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
+
+package apijson.surrealdb;
+
+import apijson.*;
+import apijson.orm.AbstractParser;
+import apijson.orm.SQLConfig;
+import com.alibaba.fastjson.JSONObject;
+import com.surrealdb.*;
+import com.surrealdb.signin.Root;
+import com.surrealdb.signin.Signin;
+
+import java.lang.Object;
+import java.sql.SQLException;
+import java.util.*;
+
+import static apijson.orm.AbstractSQLExecutor.KEY_RAW_LIST;
+
+
+/**
+ * @author Lemon
+ * @see DemoSQLExecutor 重写 execute 方法：
+ *     \@Override
+ *      public JSONObject execute(@NotNull SQLConfig<Long> config, boolean unknownType) throws Exception {
+ *          if (config.isSurrealDB()) {
+ *              return SurrealDBUtil.execute(config, null, unknownType);
+ *          }
+ *
+ *          return super.execute(config, unknownType);
+ *     }
+ *
+ *     DemoSQLConfig 重写方法 getNamespace, getSQLNamespace, getSchema, getSQLSchema 方法
+ *    \@Override
+ * 	   public String getNamespace() {
+ * 		   return SurrealDBUtil.getNamespace(super.getNamespace(), DEFAULT_NAMESPACE, isSurrealDB(    ;
+ * 	   }
+ * 	  \@Override
+ * 	   public String getSQLNamespace() {
+ * 		   return SurrealDBUtil.getSQLNamespace(super.getSQLNamespace(), isSurrealDB(    ;
+ * 	   }
+ *
+ *    \@Override
+ *     public String getSchema() {
+ * 	       return SurrealDBUtil.getSchema(super.getSchema(), DEFAULT_SCHEMA, isSurreal());
+ *     }
+ *
+ *    \@Override
+ *     public String getSQLSchema() {
+ * 		   return SurrealDBUtil.getSQLSchema(super.getSQLSchema(), isSurrealDB());
+ *     }
+ */
+public class SurrealDBUtil {
+    public static final String TAG = "SurrealDBUtil";
+
+    public static String getNamespace(String namespace, String defaultNamespace) {
+        return getNamespace(namespace, defaultNamespace, true);
+    }
+    public static String getNamespace(String namespace, String defaultNamespace, boolean isSurrealDB) {
+        if (isSurrealDB && StringUtil.isEmpty(namespace)) {
+            namespace = defaultNamespace;
+        }
+        return namespace;
+    }
+
+    public static String getSQLNamespace(String namespace) {
+        return getSQLNamespace(namespace, true);
+    }
+    public static String getSQLNamespace(String namespace, boolean isSurrealDB) {
+        return isSurrealDB ? null : namespace;
+    }
+
+    public static String getSchema(String schema, String defaultSchema) {
+        return getSchema(schema, defaultSchema, true);
+    }
+    public static String getSchema(String schema, String defaultSchema, boolean isSurrealDB) {
+        if (isSurrealDB && StringUtil.isEmpty(schema)) {
+            schema = defaultSchema;
+        }
+        return schema;
+    }
+
+    public static String getSQLSchema(String schema) {
+        return getSQLSchema(schema, true);
+    }
+    public static String getSQLSchema(String schema, boolean isSurrealDB) {
+        return isSurrealDB ? null : schema;
+    }
+
+    public static <T> String getClientKey(@NotNull SQLConfig<T> config) {
+        String uri = config.getDBUri();
+        return uri + (uri.contains("?") ? "&" : "?") + "username=" + config.getDBAccount();
+    }
+
+    public static final Map<String, Surreal> CLIENT_MAP = new LinkedHashMap<>();
+    public static <T> Surreal getClient(@NotNull SQLConfig<T> config) {
+        return getClient(config, true);
+    }
+    public static <T> Surreal getClient(@NotNull SQLConfig<T> config, boolean autoNew) {
+        String key = getClientKey(config);
+
+        Surreal client = CLIENT_MAP.get(key);
+        if (autoNew && client == null) {
+            client = new Surreal();
+            client.connect(config.getDBUri());
+            Signin signin = new Root(config.getDBAccount(), config.getDBPassword());
+            client.signin(signin);
+            client.useNs(config.getNamespace());
+            client.useDb(config.getSchema());
+
+            CLIENT_MAP.put(key, client);
+        }
+
+        return client;
+    }
+
+    public static <T> void closeClient(@NotNull SQLConfig<T> config) {
+        Surreal client = getClient(config, false);
+        if (client != null) {
+            String key = getClientKey(config);
+            CLIENT_MAP.remove(key);
+
+//            try {
+            client.close();
+//            }
+//            catch (Throwable e) {
+//                e.printStackTrace();
+//            }
+        }
+    }
+
+    public static <T> void closeAllClient() {
+        Collection<Surreal> cs = CLIENT_MAP.values();
+        for (Surreal c : cs) {
+            try {
+                c.close();
+            }
+            catch (Throwable e) {
+                e.printStackTrace();
+            }
+        }
+
+        CLIENT_MAP.clear();
+    }
+
+
+    public static <T> JSONObject execute(@NotNull SQLConfig<T> config, String sql, boolean unknownType) throws Exception {
+        if (RequestMethod.isQueryMethod(config.getMethod())) {
+            List<JSONObject> list = executeQuery(config, sql, unknownType);
+            JSONObject result = list == null || list.isEmpty() ? null : list.get(0);
+            if (result == null) {
+                result = new JSONObject(true);
+            }
+
+            if (list != null && list.size() > 1) {
+                result.put(KEY_RAW_LIST, list);
+            }
+
+            return result;
+        }
+
+        return executeUpdate(config, sql);
+    }
+
+    public static <T> int execUpdate(SQLConfig<T> config, String sql) throws Exception {
+        JSONObject result = executeUpdate(config, sql);
+        return result.getIntValue(JSONResponse.KEY_COUNT);
+    }
+
+    public static <T> JSONObject executeUpdate(SQLConfig<T> config, String sql) throws Exception {
+        return executeUpdate(null, config, sql);
+    }
+    public static <T> JSONObject executeUpdate(Surreal client, SQLConfig<T> config, String sql) throws Exception {
+        if (client == null) {
+            client = getClient(config);
+        }
+
+//       TODO client.update(StringUtil.isEmpty(sql) ? config.getSQL(false) : sql);
+
+        JSONObject result = AbstractParser.newSuccessResult();
+
+        RequestMethod method = config.getMethod();
+        if (method == RequestMethod.POST) {
+            List<List<Object>> values = config.getValues();
+            result.put(JSONResponse.KEY_COUNT, values == null ? 0 : values.size());
+        } else {
+            String idKey = config.getIdKey();
+            Object id = config.getId();
+            Object idIn = config.getIdIn();
+            if (id != null) {
+                result.put(idKey, id);
+            }
+            if (idIn != null) {
+                result.put(idKey + "[]", idIn);
+            }
+
+            if (method == RequestMethod.PUT) {
+                Map<String, Object> content = config.getContent();
+                result.put(JSONResponse.KEY_COUNT, content == null ? 0 : content.size());
+            } else {
+                result.put(JSONResponse.KEY_COUNT, id == null && idIn instanceof Collection ? ((Collection<?>) idIn).size() : 1); // FIXME 直接 SQLAuto 传 Flux/InfluxQL INSERT 如何取数量？
+            }
+        }
+
+        return result;
+    }
+
+
+    public static JSONObject execQuery(@NotNull SQLConfig<?> config, String sql, boolean unknownType) throws Exception {
+        List<JSONObject> list = executeQuery(config, sql, unknownType);
+        JSONObject result = list == null || list.isEmpty() ? null : list.get(0);
+        if (result == null) {
+            result = new JSONObject(true);
+        }
+
+        if (list != null && list.size() > 1) {
+            result.put(KEY_RAW_LIST, list);
+        }
+
+        return result;
+    }
+
+    public static List<JSONObject> executeQuery(@NotNull SQLConfig<?> config, String sql, boolean unknownType) throws Exception {
+        return executeQuery(null, config, sql, unknownType);
+    }
+    public static List<JSONObject> executeQuery(Surreal client, @NotNull SQLConfig<?> config, String sql, boolean unknownType) throws Exception {
+        if (client == null) {
+            client = getClient(config);
+        }
+
+        Response response = client.query(sql);
+        Array arr = response.take(0).getArray();
+
+        List<JSONObject> resultList = new ArrayList<>();
+
+        for (int i = 0; i < arr.len(); i++) {
+            Value val = arr.get(i);
+            JSONObject obj = new JSONObject(true);
+
+            //TODO 递归处理所有类型
+            if (val.isObject()) {
+                com.surrealdb.Object o = val.getObject();
+                for (Entry ety : o) {
+                    Value v = ety.getValue();
+//                    obj.put(ety.getKey(), JSON.parse(v.toString()));
+                    obj.put(ety.getKey(), v.isNull() ? null : JSON.parse(v.toString()));
+                }
+            } else if (val.isArray()) {
+                Array arr2 = val.getArray();
+                for (int j = 0; j < arr2.len(); j++) {
+                    Value v = arr2.get(j);
+                }
+            } else {
+                obj.put("value", val.isNull() ? null : val.toString());
+            }
+
+            resultList.add(obj);
+        }
+
+        return resultList;
+    }
+
+
+}
